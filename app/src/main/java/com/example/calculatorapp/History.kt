57d35dfd61fd7b083a3440cfd70e0793521e0da2
kazilfr <kazilfr@gmail.com>
2024-02-25 17:45:17 -0500

    Add back button

--- app/src/main/java/com/example/calculatorapp/History.kt
+++ app/src/main/java/com/example/calculatorapp/History.kt
@@ -10,13 +10,17 @@ import android.widget.TextView
 
 class History : AppCompatActivity() {
     private lateinit var tvHistory:TextView
+    private  lateinit var btBack: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
-
+        btBack = findViewById(R.id.btnBack)
         tvHistory = findViewById(R.id.tvHistory)
 
+        btBack.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+        }
         val item = intent.getStringExtra("RESULT")
 
         tvHistory.setText(item)
